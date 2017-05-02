d46b01613ac429619f0de6a35c958915e49ed8a5
massdriver <massdriverxxx@gmail.com>
2017-05-02 20:15:47 +0400

    custom network

---
+++ Assets/Multiverse/Scripts/UNetworkIdentity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Multiverse
+{
+    public sealed class UNetworkIdentity : MonoBehaviour
+    {
+        public string assetNickname;
+
+        [SerializeField]
+        private ulong m_AssetId;
+
+        public ulong assetId { get { return m_AssetId; } }
+
+        public const ulong ServerOwner = ulong.MaxValue;
+        public const ulong InvalidNetId = ulong.MaxValue;
+
+        public ulong netId { get; private set; }
+        public ulong ownerId { get; private set; }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            m_AssetId = HashUtil.FromString64(assetNickname);
+        }
+#endif
+    }
+}
---
+++ Assets/Multiverse/Scripts/UNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Multiverse
+{
+    public class UNetworkManager : MonoBehaviour
+    {
+        public int serverPort = 14678;
+        public int maxServerConnections = 32;
+        public string sessionName = "unet";
+
+        public string networkSceneName { get; private set; }
+        public ulong localOwnerId { get; private set; }
+        public GameObject localPlayerObject { get; private set; }
+        public bool isServer { get; private set; }
+        public bool isClient { get; private set; }
+        public bool isPureClient { get { return !isServer && isClient; } }
+
+        private Dictionary<ulong, UNetworkIdentity> networkObjects { get; set; }
+        private Dictionary<ulong, UNetworkIdentity> sceneInitialObjects { get; set; }
+        private Dictionary<ulong, GameObject> playerObjects { get; set; }
+
+    }
+}
