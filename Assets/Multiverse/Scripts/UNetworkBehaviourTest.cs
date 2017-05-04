77add231134f6cccb30b8f7a7b89f6ef4ecdf7c1
massdriver <massdriverxxx@gmail.com>
2017-05-04 15:20:38 +0400

    debug

---
+++ Assets/Multiverse/Scripts/UNetworkBehaviourTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+using UnityEngine;
+
+namespace Multiverse
+{
+    internal class MsgTest : Message
+    {
+        public int i;
+
+        public override void Read(NetBuffer msg)
+        {
+            i = msg.ReadInt32();
+        }
+
+        public override void Write(NetBuffer msg)
+        {
+            msg.Write(i);
+        }
+    }
+
+    public sealed class UNetworkBehaviourTest : UNetworkBehaviour
+    {
+        public float value;
+
+        private void Start()
+        {
+            SetMessageHandler<MsgTest>(HandleMsgTest);
+        }
+
+        private void HandleMsgTest(Message m)
+        {
+            MsgTest msg = m as MsgTest;
+            Debug.Log("HandleMsgTest=" + msg.i);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!hasAuthority)
+                return;
+
+            if(isServer)
+            {
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    MsgTest m = new MsgTest();
+                    m.i = 456;
+                    SendClientRpc(m);
+                }
+            }
+        }
+    }
+}
